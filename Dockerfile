77845c65d291489c1f3a5deae1febbd6027a38d9
ikarodvd <ikaromiguel13@gmail.com>
2023-10-24 17:24:34 -0400

    initial

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 # Use a imagem oficial do Node.js
-FROM node:14
+FROM node:20.5.1
+
+# Instale o utilitário ping
+RUN apt-get update && apt-get install -y iputils-ping
 
 # Defina o diretório de trabalho no contêiner
 WORKDIR /app
@@ -11,6 +14,8 @@ COPY src ./src
 
 # Instale as dependências
 RUN npm install
+ADD https://github.com/vishnubob/wait-for-it/raw/master/wait-for-it.sh /wait-for-it.sh
+RUN chmod +x /wait-for-it.sh
 
 # Exponha a porta em que o aplicativo Nest.js será executado
 EXPOSE 3000
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './Module/usuario/usuario.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'zanetti',
+      database: 'postgres',
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
